57fb869647d8ec584553fd4943d697cfe39ddcff
photiary <54730417+photiary@users.noreply.github.com>
2025-06-14 18:30:42 +0900

    Initial commit

---
+++ src/test/java/com/funa/SpringBootStarterKitApplicationTests.java
@@ -0,0 +1,11 @@
+package com.funa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterKitApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
